f3a1c230457c958c2df7cf4f23b1984f2424a12a
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-07 17:10:21 +0100

    feat: snack 2 completato

--- snack-1/app.js
+++ snack-1/app.js
@@ -2,7 +2,7 @@
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
 
-const bike = [{nome:'mountainbike', peso: 20 },{nome:'tandem', peso: 35}]
+const bike = [{nome:'mountainbike', peso: '20kg'},{nome:'tandem', peso: '30kg'}]
 let bikeName = bike[0].nome
 console.log(bikeName)
 let bikeWeight = bike[0].peso
--- snack-2/app.js
+++ snack-2/app.js
@@ -3,3 +3,28 @@ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+const championship =[{
+    nome: 'Inter',
+    punti: 0,
+    falli: 0
+},
+{   nome: 'Roma',
+    punti: 0,
+    falli: 0
+}]
+
+// generazione punti e falli 
+championship[0].punti = Math.floor(Math.random()*50)+1
+championship[0].falli = Math.floor(Math.random()*50)+1
+championship[1].punti = Math.floor(Math.random()*50)+1
+championship[1].falli = Math.floor(Math.random()*50)+1
+console.log(championship)
+
+// generazione nuovo array
+const championshipFouls = []
+championshipFouls.push(championship[0].nome)
+championshipFouls.push(championship[0].falli)
+championshipFouls.push(championship[1].nome)
+championshipFouls.push(championship[1].falli)
+console.log(championshipFouls)
