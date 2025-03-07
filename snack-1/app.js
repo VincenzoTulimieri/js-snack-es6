d5cd8cb4df0fc6915df9472b4cf942bdbb07a699
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-07 16:27:10 +0100

    feat: snack 1 fatto

--- snack-1/app.js
+++ snack-1/app.js
@@ -1,3 +1,12 @@
 /*Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.*/
+
+const bike = [{nome:'mountainbike', peso: 20 },{nome:'tandem', peso: 35}]
+let bikeName = bike[0].nome
+console.log(bikeName)
+let bikeWeight = bike[0].peso
+console.log(bikeWeight)
+console.log(bike[0])
+let nameAndWeight = bikeName + ' ' + bikeWeight
+console.log(nameAndWeight)
