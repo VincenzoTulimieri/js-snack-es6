874ba6cfe827074f0d346ccc63594ccf71515491
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-10 13:44:36 +0100

    fix: snack corretto

--- snack-2/app.js
+++ snack-2/app.js
@@ -4,27 +4,34 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
 
-const championship =[{
-    nome: 'Inter',
-    punti: 0,
-    falli: 0
-},
-{   nome: 'Roma',
-    punti: 0,
-    falli: 0
-}]
+const championship =[
+    {nome: 'Inter',punti: 0,falli: 0},
+    {nome: 'Roma',punti: 0,falli: 0}
+]
 
 // generazione punti e falli 
-championship[0].punti = Math.floor(Math.random()*50)+1
-championship[0].falli = Math.floor(Math.random()*50)+1
-championship[1].punti = Math.floor(Math.random()*50)+1
-championship[1].falli = Math.floor(Math.random()*50)+1
+for(let i = 0; i < championship.length; i++){
+    let squad = championship[i];
+
+    squad.punti = Math.floor(Math.random()*50)+1
+    squad.falli = Math.floor(Math.random()*50)+1
+
+
+}
 console.log(championship)
 
 // generazione nuovo array
+
 const championshipFouls = []
-championshipFouls.push(championship[0].nome)
-championshipFouls.push(championship[0].falli)
-championshipFouls.push(championship[1].nome)
-championshipFouls.push(championship[1].falli)
+
+for(let i = 0; i < championship.length; i++){
+
+    const squad = championship[i];
+    const newData = {
+        nome: squad.nome,
+        falli: squad.falli
+    }
+    championshipFouls.push(newData)
+
+}
 console.log(championshipFouls)
